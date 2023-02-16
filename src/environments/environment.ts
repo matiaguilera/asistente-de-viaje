667143645257a59edfa5dda0c3645d7feca53637
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2023-02-16 14:15:13 -0300

    Agrega variables para live deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  mapBoxToken:
+    'pk.eyJ1IjoibWF0aWFndWlsZXJhIiwiYSI6ImNsZHVlOXFqZTA0emczbnA5eWxmaWNia2sifQ.G7L0t9gYA7XpsLuiEuVu1g',
+  firebase: {
+    apiKey: 'AIzaSyAuXJ2dSSObjpdyxxP90p3zfTOKsAtEJtU',
+    authDomain: 'asistente-de-viaje.firebaseapp.com',
+    projectId: 'asistente-de-viaje',
+    storageBucket: 'asistente-de-viaje.appspot.com',
+    messagingSenderId: '505403232408',
+    appId: '1:505403232408:web:7ab48f62dfd18eed1a00ed',
+  },
+};
