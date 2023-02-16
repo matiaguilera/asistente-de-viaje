0d63218a0cd9f42fbdd9b3f8a57f519977ed280d
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2023-02-16 15:50:16 -0300

    Instala y configura Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@ export const environment = {
   mapBoxToken:
     'pk.eyJ1IjoibWF0aWFndWlsZXJhIiwiYSI6ImNsZHVlOXFqZTA0emczbnA5eWxmaWNia2sifQ.G7L0t9gYA7XpsLuiEuVu1g',
   firebase: {
+    projectId: 'asistente-de-viaje',
+    appId: '1:505403232408:web:7ab48f62dfd18eed1a00ed',
+    storageBucket: 'asistente-de-viaje.appspot.com',
     apiKey: 'AIzaSyAuXJ2dSSObjpdyxxP90p3zfTOKsAtEJtU',
     authDomain: 'asistente-de-viaje.firebaseapp.com',
-    projectId: 'asistente-de-viaje',
-    storageBucket: 'asistente-de-viaje.appspot.com',
     messagingSenderId: '505403232408',
-    appId: '1:505403232408:web:7ab48f62dfd18eed1a00ed',
   },
 };
